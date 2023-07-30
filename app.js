f6864323ea442f8beaab92a482cdf5ec49b454ee
binhhp <Binhhp20@gmail.com>
2023-07-30 17:08:15 +0700

    add to cart

--- app.js
+++ app.js
@@ -1,7 +1,69 @@
 
-// let iconDropdown = document.getElementById('icon-dropdown')
-// let dropdown  = document.getElementById('dropdown')
+// function addToCart(idSanPhamm, tenSanPham, giaSanPham, hinhAnhSanPham, soLuongSanPham) {
+//     //them vao gio hang
+//     debugger
+//     var cartItems = [];
+//     if(localStorage.getItem('productsInCart')){
+//         cartItems = JSON.parse(localStorage.getItem('productsInCart'));
+//     }
+    
+//     if(cartItems.filter(x => x.id === idSanPhamm).length > 0){
+//         var index = cartItems.findIndex(x => x.id === idSanPhamm);
+//         cartItems[index].quantity += soLuongSanPham;
+//     }
+//     else{
+//         cartItems.push({
+//             id: idSanPhamm,
+//             name: tenSanPham,
+//             price: giaSanPham,
+//             image: hinhAnhSanPham,
+//             quantity: soLuongSanPham
+//         });
+//     }
+//     localStorage.setItem('productsInCart', JSON.stringify(cartItems));
+//     document.getElementById('cart').innerHTML = `${cartItems.map(item => `<a>${item.name}: ${item.quantity}</a>`)}`;   
+// }
 
-// iconDropdown.addEventListener('click', function(){
-//     dropdown.style.display = 'block'
-// })
+// (function(){
+//     var cartItems = [];
+//     if(localStorage.getItem('productsInCart')){
+//         cartItems = JSON.parse(localStorage.getItem('productsInCart'));
+//     }
+//     document.getElementById('cart').innerHTML = `${cartItems.map(item => `<a>${item.name}: ${item.quantity}</a>`)}`;
+// })()
+
+
+
+
+function addToCart(idSanPhamm, tenSanPham, giaSanPham, hinhAnhSanPham, soLuongSanPham) {
+    //them vao gio hang
+    
+    var cartItems = [];
+    if(localStorage.getItem('productsInCart')){
+        cartItems = JSON.parse(localStorage.getItem('productsInCart'));
+    }
+    
+    if(cartItems.filter(x => x.id === idSanPhamm).length < 0){
+        var index = cartItems.findIndex(x => x.id === idSanPhamm);
+        cartItems[index].quantity -= soLuongSanPham;
+    // }
+    // else{
+    //     cartItems.push({
+    //         id: idSanPhamm,
+    //         name: tenSanPham,
+    //         price: giaSanPham,
+    //         image: hinhAnhSanPham,
+    //         quantity: soLuongSanPham
+    //     });
+    }
+    localStorage.setItem('productsInCart', JSON.stringify(cartItems));
+    document.getElementById('cart').innerHTML = `${cartItems.map(item => `<a>${item.name}: ${item.quantity}</a>`)}`;   
+}
+
+(function(){
+    var cartItems = [];
+    if(localStorage.getItem('productsInCart')){
+        cartItems = JSON.parse(localStorage.getItem('productsInCart'));
+    }
+    document.getElementById('cart').innerHTML = `${cartItems.map(item => `<a>${item.name}: ${item.quantity}</a>`)}`;
+})()
