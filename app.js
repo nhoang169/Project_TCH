0786c313461a555effa22f065dc216d9bc214fab
nhoang169 <hoang.1699.hp@gmail.com>
2023-07-30 15:15:37 +0700

    first project

---
+++ app.js
@@ -0,0 +1,7 @@
+
+// let iconDropdown = document.getElementById('icon-dropdown')
+// let dropdown  = document.getElementById('dropdown')
+
+// iconDropdown.addEventListener('click', function(){
+//     dropdown.style.display = 'block'
+// })
